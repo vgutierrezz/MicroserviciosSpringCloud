ff14613f66108931f0e2f887ce34d06c60dc2acb
vgutierrezz <gutierrezvalentina14@gmail.com>
2026-01-14 16:22:56 -0300

    Configuración para un microservicio con eureka

---
+++ product-service/product-service/src/test/java/com/vgutierrez/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vgutierrez.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
