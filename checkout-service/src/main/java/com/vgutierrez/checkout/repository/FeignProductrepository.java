8ca2006ffb0c3205e03caaaa0a2a5b530d68623b
vgutierrezz <gutierrezvalentina14@gmail.com>
2026-01-20 15:45:29 -0300

    Load Balancer Random

--- checkout-service/src/main/java/com/vgutierrez/checkout/repository/FeignProductrepository.java
+++ checkout-service/src/main/java/com/vgutierrez/checkout/repository/FeignProductrepository.java
@@ -1,13 +1,16 @@
 package com.vgutierrez.checkout.repository;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.vgutierrez.checkout.configuration.LoadBalancerConfiguration;
 import com.vgutierrez.checkout.model.dto.Product;
 
 @FeignClient(name = "product-service")
+@LoadBalancerClient(name = "product-service", configuration = LoadBalancerConfiguration.class)
 public interface FeignProductrepository {
 
     @RequestMapping(method = RequestMethod.GET, value = "/products")
