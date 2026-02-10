a7d593035124c13f3b1e987d504d7eef3462dd32
vgutierrezz <gutierrezvalentina14@gmail.com>
2026-02-10 14:45:52 -0300

    Ruteo con Gateway

--- eureka-server/src/main/java/com/example/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.eureka_server;
+package com.vgutierrez.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
